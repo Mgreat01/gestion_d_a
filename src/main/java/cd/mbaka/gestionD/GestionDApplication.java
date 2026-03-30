de8d5e953191f8cdfe506426d2b121ca07d67143
Mgreat01 <ephraimmonga5@gmail.com>
2026-03-30 02:39:45 +0200

    feat : gestion du  LocalDateTime

--- src/main/java/cd/mbaka/gestionD/GestionDApplication.java
+++ src/main/java/cd/mbaka/gestionD/GestionDApplication.java
@@ -1,13 +1,22 @@
 package cd.mbaka.gestionD;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestionDApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(GestionDApplication.class, args);
 	}
 
+	@Bean
+	ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		return mapper;
+	}
 }
